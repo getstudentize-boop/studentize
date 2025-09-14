037271315bb06f87d14756daeee256044c9c891f
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2025-09-14 16:33:07 +0200

    add logs

--- apps/web/src/utils/workos.ts
+++ apps/web/src/utils/workos.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@workos-inc/authkit-js";
 
 export const getUserAuth = async () => {
+  console.log("Getting user auth...", import.meta.env.VITE_WORKOS_CLIENT_ID);
   const authkit = await createClient(import.meta.env.VITE_WORKOS_CLIENT_ID);
   return authkit.getUser();
 };
