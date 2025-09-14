c1b7a97016730d164f1f8403f68318475d7c1c34
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2025-09-14 18:34:43 +0200

    set api hostname

--- apps/web/src/utils/workos.ts
+++ apps/web/src/utils/workos.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@workos-inc/authkit-js";
 
 export const getUserAuth = async () => {
-  console.log("Getting user auth...", import.meta.env.VITE_WORKOS_CLIENT_ID);
-  const authkit = await createClient(import.meta.env.VITE_WORKOS_CLIENT_ID);
+  const authkit = await createClient(import.meta.env.VITE_WORKOS_CLIENT_ID, {
+    apiHostname: "auth.workos.com",
+  });
   return authkit.getUser();
 };
