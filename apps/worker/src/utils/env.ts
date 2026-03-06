a01205fc0fad6511f42ffdead7c3b23df70b5952
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2026-03-06 19:53:31 +0200

    add email

--- apps/worker/src/utils/env.ts
+++ apps/worker/src/utils/env.ts
@@ -2,4 +2,7 @@ import { Env as HonoEnv } from "hono";
 
 export type Env = HonoEnv & {
   TRANSCRIPTIONS: R2Bucket;
+  RESEND_API_KEY?: string;
+  RESEND_FROM_EMAIL?: string;
+  RESEND_FROM_NAME?: string;
 };
