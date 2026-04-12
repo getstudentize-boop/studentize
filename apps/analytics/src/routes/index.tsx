939971ad2be17a81603809c207b45dcc97eed972
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2026-04-12 22:21:54 +0200

    update

--- apps/analytics/src/routes/index.tsx
+++ apps/analytics/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+
 import { ActiveStudents } from "#/stats/active-students";
 import { ConversionRate } from "#/stats/conversion-rate";
 import { FeatureAdoption } from "#/stats/feature-adoption";
